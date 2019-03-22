b2d855f8b0fcb8ad00c9ec678f978b48932ca660
Bornce <33858196+Bornce@users.noreply.github.com>
2019-03-22 14:42:40 +0800

    加入电磁铁控制代码
    加入了电磁铁控制代码

--- TemperaturesCollector_ArduinoNano.ino
+++ TemperaturesCollector_ArduinoNano.ino
@@ -1,26 +1,38 @@
 /*
  * 芯片选择Arduino Nano,ATmage328P Old Bootloader  AVRISP mkll
- * 
+ * A0通道必须放在相变材料上
  * 
 */
-
+/***********************************************************************
+ * include 区域
+************************************************************************/
 #include  "string.h"
 #include <MsTimer2.h>
 #include <stdio.h>
 #include "Arduino.h"
 
 
+
+/***********************************************************************
+ * include 区域
+************************************************************************/
 #define LEDPIN  13
+#define	P_CTRL_PIN	2
+#define	N_CTRL_PIN	3
+#define	SET_MAX_TEMPERATURE_IN_BOX	1
+#define	SET_MIN_TEMPERATURE_IN_BOX	-1
+#define	PULL_MAGNIC_HANDSPIKE()		digitalWrite(P_CTRL_PIN, HIGH);digitalWrite(N_CTRL_PIN, LOW);
+#define	PULL_MAGNIC_HANDSPIKE()		digitalWrite(P_CTRL_PIN, LOW);digitalWrite(N_CTRL_PIN, HIGH);
+#define	DEFAULT_MAGNIC_HANDSPIKE()	digitalWrite(P_CTRL_PIN, LOW);digitalWrite(N_CTRL_PIN, LOW);
 
-
-
-const  char  ServerCMD[]={"UPDATE THE DATA PLEASE"};
-
+/***********************************************************************
+ * 自定义数据类型 区域
+************************************************************************/
 enum enumStruct_TypeOfNTC
 {
-  Res_2K_B3500K = 0,
-  Res_5K_B3700K ,
-  Res_10K_B3380K  ,
+//  Res_2K_B3500K = 0,
+//  Res_5K_B3700K ,
+//  Res_10K_B3380K  ,
   Res_10K_B3900K  ,
   TotalType_Res,
 };
@@ -44,47 +56,61 @@ struct  CUNTIAN_NTC_TABLE
 };
 //
 
+/***********************************************************************
+ * 常量声明 区域
+************************************************************************/
+const  char  ServerCMD[]={"UPDATE THE DATA PLEASE"};
+
 const  struct  CUNTIAN_NTC_TABLE NTC[]=
 {
 //temperature,Res_2K_B3500K,Res_5K_B3700K......
-{-40, {44.657,  123.484,  195.652,  347.808}},
-{-35, {33.505,  92.295,   148.171,  248.591}},
-{-30, {25.388,  69.614,   113.347,  179.973}},
-{-25, {19.402,  52.86,    87.559,   131.832 }},
-{-20, {14.961,  40.48,    68.237,   97.679}},
-{-15, {11.644,  31.275,   53.65,    73.119 }},
-{-10, {9.133,   24.339,   42.506,   55.301}},
-{-5,  {7.198,   19.154,   33.892,   42.257 }},
-{0,   {5.716,   15.148,   27.219,   32.582 }},
-{5,   {4.571,   11.964,   22.021,   25.324 }},
-{10,  {3.682,   9.52,     17.926,   19.847 }},
-{15,  {2.987,   7.624,    14.674,   15.679 }},
-{20,  {2.437,   6.16,     12.081,   12.478}},
-{25,  {2.0,     5.0,      10.0,     10.0   }},
-{30,  {1.651,   4.082,    8.315,    8.068  }},
-{35,  {1.371,   3.354,    6.948,    6.552   }},
-{40,  {1.143,   2.773,    5.834,    5.353  }},
-{45,  {0.958,   2.299,    4.917,    4.399  }},
-{50,  {0.807,   1.914,    4.161,    3.635  }},
-{55,  {0.683,   1.607,    3.535,    3.02  }},
-{60,  {0.582,   1.356,    3.014,    2.521  }},
-{65,  {0.497,   1.149,    2.586,    2.115  }},
-{70,  {0.426,   0.978,    2.228,    1.783  }},
-{75,  {0.367,   0.834,    1.925,    1.51  }},
-{80,  {0.318,   0.714,    1.669,    1.284  }},
-{85,  {0.276,   0.612,    1.452,    1.096  }},
-{90,  {0.24,    0.527,    1.268,    0.939   }},
-{95,  {0.21,    0.456,    1.11,     0.808   }},
-{100, {0.183,   0.396,    0.974,    0.698   }},
-{105, {0.161,   0.345,    0.858,    0.605  }},
-{110, {0.142,   0.302,    0.758,    0.527  }},
-{115, {0.125,   0.264,    0.671,    0.46  }},
-{120, {0.111,   0.232,    0.596,    0.403 }},
-{125, {0.099,   0.205,    0.531,    0.354}}
+{-40, {  347.808}},
+{-35, {  248.591}},
+{-30, {  179.973}},
+{-25, {  131.832 }},
+{-20, {  97.679}},
+{-15, {  73.119 }},
+{-10, {  55.301}},
+{-5,  {  42.257 }},
+{0,   {  32.582 }},
+{5,   {  25.324 }},
+{10,  {  19.847 }},
+{15,  {  15.679 }},
+{20,  {  12.478}},
+{25,  {  10.0   }},
+{30,  {  8.068  }},
+{35,  {  6.552   }},
+{40,  {  5.353  }},
+{45,  {  4.399  }},
+{50,  {  3.635  }},
+{55,  {  3.02  }},
+{60,  {  2.521  }},
+{65,  {  2.115  }},
+{70,  {  1.783  }},
+{75,  {  1.51  }},
+{80,  {  1.284  }},
+{85,  {  1.096  }},
+{90,  {  0.939   }},
+{95,  {  0.808   }},
+{100, {  0.698   }},
+{105, {  0.605  }},
+{110, {  0.527  }},
+{115, {  0.46  }},
+{120, {  0.403 }},
+{125, {  0.354}}
 };
 //
 
+/***********************************************************************
+ * global parameters 区域
+************************************************************************/
+int lenOfUartRev  = 0;
+float temperatureBuff[8]={0.0};
 
+
+/***********************************************************************
+ * function blocks 区域
+************************************************************************/
 
 float GetTemperatureFromNTC(float res,unsigned  char  TypeOfRes)
 {
@@ -125,103 +151,41 @@ float GetTemperatureFromNTC(float res,unsigned  char  TypeOfRes)
 //
 
 
-//int serial_putc( char c, struct __file * )
-//{
-//  Serial.write( c );
-//  return c;
-//}
-//void printf_begin(void)
-//{
-//  fdevopen( &serial_putc, 0 );
-//}
-////
 
-int lenOfUartRev  = 0;
-float temperatureBuff[8]={0.0};
-
-void UpdateADCs();
-
-
-
-void onTimer() 
+static int partition(float* sortArray, int low, int height)
 {
-  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
-}
-//
+  float key = sortArray[low];//
 
-void  setup(void)
-{
-  Serial.begin(115200);
-  pinMode(LEDPIN, OUTPUT);
-
-  MsTimer2::set(200, onTimer); //设置中断，每1000ms进入一次中断服务程序 onTimer()
-  MsTimer2::start(); //开始计时
-
-  Serial.print("Reboot\r\n");
-}
-//
-
-void  loop()// run over and over
-{
- 
-
-  
-  UpdateADCs();
-
-{
-  for(int channelIndex=0;channelIndex<8;channelIndex++)
+  while (low < height)
   {
-    temperatureBuff[channelIndex] = GetTemperatureFromNTC(temperatureBuff[channelIndex],Res_10K_B3900K);
+    while (low < height && sortArray[height] >= key)
+      height--;// 
+    sortArray[low] = sortArray[height];// 
+    while (low < height && sortArray[low] <= key)
+      low++;// 
+    sortArray[height] = sortArray[low];//
+  }
+  sortArray[low] = key;// 
+
+  return low;
+}
+//
+
+
+
+
+void quicksort(float* sortArray, int low, int height)
+{
+  if (low < height)
+  {
+    int result = partition(sortArray, low, height);
+
+    quicksort(sortArray, low, result - 1);
+    quicksort(sortArray, result + 1, height);
   }
   //
-
 }
 //
-
-
-  if(Serial.available())
-  {
-    int index = 0;
-    char  UartGet[512]={0};
-    
-    //
-    index = 0;
-    lenOfUartRev  = 0;
-    lenOfUartRev = Serial.available();
-
-    while(lenOfUartRev--) 
-    {
-      UartGet[index] = Serial.read();
-      index ++;
-    }
-    //
-    
-    
-    if(memcmp(UartGet,ServerCMD,sizeof(ServerCMD)-1) == 0)
-    {
-      for(int channelIndex=0;channelIndex<8;channelIndex++)
-      {
-        Serial.print(temperatureBuff[channelIndex],3);
-        Serial.print(",");
-      }
-      //
-      Serial.print("\r\n");
-       
-    }
-    //
-    
-    memset(UartGet,0,sizeof(UartGet));
-    
-  }
-  //
-
-
-  
-  delay(300);
-}
-//
-
-
 
 
 void  UpdateADCs()
@@ -266,3 +230,184 @@ void  UpdateADCs()
 
 }
 //
+
+
+void magnicAction(char CMD)
+{
+	switch(CMD)
+	{
+		case	0:
+		PULL_MAGNIC_HANDSPIKE();
+		break;
+		
+		case	1:
+		PUSH_MAGNIC_HANDSPIKE();
+		break;
+		
+		default:
+		DEFAULT_MAGNIC_HANDSPIKE();
+		break;
+	}
+	//
+}
+//
+
+void onTimer() 
+{
+  digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+}
+//
+
+
+
+void  setup(void)
+{
+	
+	pinMode(P_CTRL_PIN,OUTPUT);
+	pinMode(N_CTRL_PIN,OUTPUT);
+
+	
+	Serial.begin(115200);
+	pinMode(LEDPIN, OUTPUT);
+	
+	MsTimer2::set(200, onTimer); //设置中断，每1000ms进入一次中断服务程序 onTimer()
+	MsTimer2::start(); //开始计时
+	
+	Serial.print("Reboot\r\n");
+}
+//
+
+
+//
+void  loop()// run over and over
+{
+ 
+
+	/*硬件电阻值采集*/
+	UpdateADCs();
+
+	/*电阻值和温度值转换*/
+	{
+	  for(int channelIndex=0;channelIndex<8;channelIndex++)
+	  {
+		temperatureBuff[channelIndex] = GetTemperatureFromNTC(temperatureBuff[channelIndex],Res_10K_B3900K);
+	  }
+	  //
+
+	}
+	//
+
+	/*收到上位机命令*/
+	if(Serial.available())
+	{
+		int index = 0;
+		char  UartGet[512]={0};
+		
+		//
+		index = 0;
+		lenOfUartRev  = 0;
+		lenOfUartRev = Serial.available();
+		
+		while(lenOfUartRev--) 
+		{
+			UartGet[index] = Serial.read();
+			index ++;
+		}
+		//
+		
+		
+		if(memcmp(UartGet,ServerCMD,sizeof(ServerCMD)-1) == 0)
+		{
+			for(int channelIndex=0;channelIndex<8;channelIndex++)
+			{
+			Serial.print(temperatureBuff[channelIndex],3);
+			Serial.print(",");
+			}
+			//
+			Serial.print("\r\n");
+			
+		}
+		//
+		
+		memset(UartGet,0,sizeof(UartGet));
+		
+	}
+	//
+
+	//对温度值进行排序
+	quicksort(temperatureBuff,0,sizeof(temperatureBuff)/sizeof(float));
+	
+	
+	//
+	{
+		#define		NO_PHASE_CHANGE_MATERIAL										0
+		#define		NO_MIN_TEMPERATURE_IN_BOX_EXCEPT_PHASE_CHANGE_MATERIAL			1
+		#define		NO_SECOND_MIN_TEMPERATURE_IN_BOX_EXCEPT_PHASE_CHANGE_MATERIAL	2
+		#define		NO_SECOND_MAX_TEMPERATURE_IN_BOX_EXCEPT_PHASE_CHANGE_MATERIAL	6
+		#define		NO_MIN_TEMPERATURE_IN_BOX_EXCEPT_PHASE_CHANGE_MATERIAL			7
+		
+		enum{Enum_Pull=0,Enum_Push,Enum_Default};
+		
+		
+		float averageTemperatureInBox;
+		char	commandForMagnet	=	Enum_Pull;
+		
+		static	char	preCommandForMagnet	=	Enum_Pull;
+		
+		averageTemperatureInBox	=	0;
+		
+		for(int i=1;i<8;i++)
+		{
+			averageTemperatureInBox	+=	temperatureBuff[i];
+		}
+		//
+		
+		averageTemperatureInBox	=	averageTemperatureInBox/7;
+		
+		
+		
+		if(averageTemperatureInBox>SET_MAX_TEMPERATURE_IN_BOX)
+		{
+			commandForMagnet	=	Enum_Pull;
+		}
+		//
+		
+		if(averageTemperatureInBox<SET_MIN_TEMPERATURE_IN_BOX)
+		{
+			commandForMagnet	=	Enum_Push;
+		}
+		//
+		
+		
+		//磁铁动作
+		{
+			//计算得到的磁铁命令前后不一致，则说明磁铁的动作需要发生改变了。
+			//如果前后一致，则释放磁铁控制线圈——磁铁是保持型的所以不必担心
+			if(commandForMagnet^preCommandForMagnet)
+			{
+				magnicAction(commandForMagnet);
+				preCommandForMagnet	=	commandForMagnet;
+			}
+			//
+			else
+			{
+				magnicAction(Enum_Default);
+			}
+			//
+			
+		}
+		//
+		
+	}
+	//
+	
+	
+
+  
+	delay(300);
+}
+//
+
+
+
+
